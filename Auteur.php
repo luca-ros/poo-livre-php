143f4bd9031ba1a84e11e8f34ef7f21340bb551d
Luca Rosano <92313604+luca-ros@users.noreply.github.com>
2022-03-16 19:54:07 +0100

    init

---
+++ Auteur.php
@@ -0,0 +1,12 @@
+<?php
+
+class Auteur{
+    class Auteur{
+    private $_nom;
+    private $_prenom;
+    private $_livres;
+
+    function __construct($nom, $prenom){
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+    }
